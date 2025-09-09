df5c46883af511b0a260666f6f772fa6c41c18ad
Kanishka-Trivedi <kally651997@gmail.com>
2025-09-09 13:08:46 +0530

    Updations Made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,59 +1,4 @@
-// import Header from './components/Header'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Home from './pages/Home';
-// import Succulents from './pages/Succulents';
-// import HerbalPlants from './pages/HerbalPlants';
-// import Bonsai from './pages/Bonsai';
-// import FloweringPlants from './pages/FloweringPlants';
-// import AirPurifyingPlants from './pages/AirPurifyingPlants';
-// import Cactus from './pages/Cactus';
-// import HangingPlants from './pages/HangingPlants';
-// import Ornamental from './pages/Ornamental';
-// import ClimbersAndCreepers from './pages/ClimbersAndCreepers';
-// import FruitPlants from './pages/FruitPlants';
-// import VegetablePlants from './pages/VegetablePlants';
-// import Footer from './components/Footer';
-// import Login from './pages/Login';
-// import Signup from './pages/Signup';
-
-
-// function App() {
-
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <Header />
-//         <Routes>
-//           <Route path={"/"} element={<Home />} />
-//           <Route path="/succulents" element={<Succulents />} />
-//           <Route path="/herbal" element={<HerbalPlants />} />
-//           <Route path="/bonsai" element={<Bonsai />} />
-//           <Route path="/flowering" element={<FloweringPlants />} />
-//           <Route path="/air-purifying" element={<AirPurifyingPlants />} />
-//           <Route path="/cactus" element={<Cactus />} />
-//           <Route path="/hanging" element={<HangingPlants />} />
-//           <Route path="/ornamental" element={<Ornamental />} />
-//           <Route path="/climbers" element={<ClimbersAndCreepers />} />
-//           <Route path="/fruit-plants" element={<FruitPlants />} />
-//           <Route path="/vegetable-plants" element={<VegetablePlants />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/signup" element={<Signup />} />
-//         </Routes>
-//         <Footer />
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App
-
-
-
-
-
-
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout'; 
 import Home from './pages/Home';
@@ -70,6 +15,7 @@ import FruitPlants from './pages/FruitPlants';
 import VegetablePlants from './pages/VegetablePlants';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+
 
 function App() {
   return (
--- Frontend/src/components/ProductItem.jsx
+++ Frontend/src/components/ProductItem.jsx
@@ -5,57 +5,6 @@
 // import { FaRegHeart } from "react-icons/fa";
 // import { IoIosGitCompare } from "react-icons/io";
 // import { MdOutlineZoomOutMap } from "react-icons/md";
-
-
-// const ProductItem = () => {
-//     return (
-//         <div className='productItem shadow-lg rounded-md overflow-hidden  border-[rgba(0, 0, 0, 0.1)]'>
-//             <div className='group imgWrapper w-[100%] h-[250px] overflow-hidden rounded-t-md relative'>
-//                 <img src="https://plantorbit.com/cdn/shop/files/image_0Is.webp?v=1710876039&width=713" alt="" className='w-full' />
-
-//                 <span className='discount flex items-center absolute top-[10px] left-[10px] z-50 bg-[#4CBB17] text-white rounded-lg p-1 text-[12px] font-[500]'> 40% Off </span>
-
-//                 <div className='actions absolute top-[-200px] right-[5px] z-50 flex items-center gap-2 flex-col w-[50px] group-hover:top-[15px] transition-all duration-700 opacity-0 group-hover:opacity-100'>
-//                     <Button className='!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white !text-black hover:!bg-[#4CBB17] hover:text-white group'><FaRegHeart className='text-[18px] !text-black hover:!text-white group-hover:text-white' /></Button>
-
-//                     <Button className='!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white !text-black hover:!bg-[#4CBB17] hover:text-white group'><MdOutlineZoomOutMap className='text-[18px] !text-black hover:!text-white group-hover:text-white' /></Button>
-
-//                     <Button className='!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white !text-black hover:!bg-[#4CBB17] hover:text-white group'><IoIosGitCompare className='text-[18px] !text-black hover:!text-white group-hover:text-white' /></Button>
-//                 </div>
-//             </div>
-
-//             <div className='info p-3 py-5 bg-[#ffeac4]'>
-//                 <h3 className='text-[16px] title mt-1 mb-1 font-[500] text-[#000]'><Link to="/" className="link transition-all">Pachyphytum Oviferum Moonstone  </Link></h3>
-
-//                 <Rating name="size-small" defaultValue={4} size="small" readOnly />
-
-//                 <div className='flex items-center gap-4'>
-//                     <span className='oldPrice line-through text-gray-500 text-[15px] font-[500]'>Rs. 159.00</span>
-//                     <span className='price text-[#1e975a] text-[15px] font-[600]'>Rs. 99.00</span>
-//                 </div>
-//             </div>
-
-//         </div>
-//     )
-// }
-
-// export default ProductItem
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import Rating from '@mui/material/Rating';
-// import { Button } from '@mui/material';
-// import { FaRegHeart } from "react-icons/fa";
-// import { IoIosGitCompare } from "react-icons/io";
-// import { MdOutlineZoomOutMap } from "react-icons/md";
 // import PropTypes from 'prop-types';
 
 // const ProductItem = ({
@@ -175,9 +124,9 @@ const ProductItem = ({
     return (
         <div className='productItem shadow-lg rounded-md overflow-hidden border-[rgba(0, 0, 0, 0.1)]'>
             <div className='group imgWrapper w-[100%] h-[250px] overflow-hidden rounded-t-md relative'>
-                <img 
-                    src={image || FALLBACK_IMAGE} 
-                    alt={title} 
+                <img
+                    src={image || FALLBACK_IMAGE}
+                    alt={title}
                     className='w-full h-full object-cover'
                     onError={(e) => (e.target.src = FALLBACK_IMAGE)}
                 />
@@ -236,6 +185,7 @@ const ProductItem = ({
 }
 
 ProductItem.propTypes = {
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     rating: PropTypes.number,
